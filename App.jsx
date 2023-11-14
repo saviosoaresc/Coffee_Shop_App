b2ef044be459eefce0b48f4725ceeb75e85b29fc
Savio <saviosoares0402@gmail.com>
2023-11-14 11:56:33 -0300

    edition App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -18,24 +18,24 @@ export default function App() {
   }, [])
   return (
     <NavigationContainer>
-      <StatusBar style='light'/>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <StatusBar style='light' />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name='Tab'
           component={TabNavigator}
-          options={{animation: 'slide_from_bottom'}}>
+          options={{ animation: 'slide_from_bottom' }}>
         </Stack.Screen>
 
         <Stack.Screen
           name='Details'
           component={DetailsScreen}
-          options={{animation: 'slide_from_bottom'}}>
+          options={{ animation: 'slide_from_bottom' }}>
         </Stack.Screen>
 
         <Stack.Screen
           name='Payment'
           component={PaymentScreen}
-          options={{animation: 'slide_from_bottom'}}>
+          options={{ animation: 'slide_from_bottom' }}>
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
