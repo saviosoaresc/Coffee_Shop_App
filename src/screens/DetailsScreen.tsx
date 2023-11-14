a0a2aea3ca47d68ebbbb0244455c83e82c067f3d
Savio <saviosoares0402@gmail.com>
2023-11-14 11:57:13 -0300

    edition details

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,16 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native'
+import { useStore } from '../store/store'
+import { COLORS } from '../theme/theme';
+import ImageBackgroundInfo from '../components/ImageBackgroundInfo';
+
+
 
 const DetailsScreen = ({navigation, route}: any) => {
-  
+
+  const ItemOfIdex = useStore((state: any) => 
+    route.params.type == 'Coffee' ? state.CoffeeList : state.BeanList,
+  )[route.params.index];
+
   return (
-    <View>
-      <Text>DetailScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex}/>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}
+      >
+        <ImageBackgroundInfo
+          EnableBackHandler={true}
+          imagelink_portrait={ItemOfIdex.imagelink_portrait}
+          type={ItemOfIdex.type}
+          id={ItemOfIdex.id}
+          favourite={ItemOfIdex.favourite}
+          name={ItemOfIdex.name}
+          special_ingredient={ItemOfIdex.special_ingredient}
+          ingredients={ItemOfIdex.ingredients}
+          average_rating={ItemOfIdex.average_rating}
+          ratings_count={ItemOfIdex.ratings_count}
+          rosted={ItemOfIdex.rosted}
+          BackHandler={() => {}}
+          ToogleFavourite={() => {}}
+        />
+      </ScrollView>
     </View>
   )
 }
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ScreenContainer:{
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex:{
+    flexGrow: 1,
+  },
+})
 
 export default DetailsScreen
