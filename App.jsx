38db60470b7b01512386c9ff5567fe586b392dd5
Savio <saviosoares0402@gmail.com>
2023-11-13 23:40:46 -0300

    editon App

--- App.jsx
+++ App.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { Ionicons } from 'react-native-vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import HomeScreen from './src/screens/HomeScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import TabNavigator from './src/navigators/TabNavigator';
+import * as SplashScreen from 'expo-splash-screen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, [])
   return (
     <NavigationContainer>
       <StatusBar style='light'/>
@@ -19,6 +23,12 @@ export default function App() {
         <Stack.Screen
           name='Tab'
           component={TabNavigator}
+          options={{animation: 'slide_from_bottom'}}>
+        </Stack.Screen>
+
+        <Stack.Screen
+          name='Details'
+          component={DetailsScreen}
           options={{animation: 'slide_from_bottom'}}>
         </Stack.Screen>
 
