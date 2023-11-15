1db69aa3901845d6fd2a1d799e33fb9066ae1cd9
Savio <saviosoares0402@gmail.com>
2023-11-15 19:48:34 -0300

    edition imageback

--- src/components/ImageBackgroundInfo.tsx
+++ src/components/ImageBackgroundInfo.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { StyleSheet, Text, View, ImageProps, ImageBackground, TouchableOpacity } from 'react-native'
 import BGIcon from './BGIcon';
 import GradientBGIcon from './GradientBGIcon';
-import { COLORS, FONTSIZE, SPACING } from '../theme/theme';
-import { Entypo } from '@expo/vector-icons'
-import CustomIcon from './CustomIcon';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons' 
 
 interface ImageBackgrounInfoProps {
   EnableBackHandler: boolean;
@@ -17,7 +16,7 @@ interface ImageBackgrounInfoProps {
   ingredients: string;
   average_rating: number;
   ratings_count: string;
-  rosted: string;
+  roasted: string;
   BackHandler?: any;
   ToggleFavourite: any;
 }
@@ -33,7 +32,7 @@ const ImageBackgroundInfo: React.FC<ImageBackgrounInfoProps> = ({
   ingredients,
   average_rating,
   ratings_count,
-  rosted,
+  roasted,
   BackHandler,
   ToggleFavourite,
 }) => {
@@ -105,14 +104,42 @@ const ImageBackgroundInfo: React.FC<ImageBackgrounInfoProps> = ({
                 <Text style={styles.ItemTitleText}>{name}</Text>
                 <Text style={styles.ItemSubtitleText}>{special_ingredient}</Text>
               </View>
-              <View>
-                <View>
-                  <CustomIcon
-                    name={type == 'Bean' ? 'bean' : 'beans'}
-                    size={type == 'Bean' ? FONTSIZE.size_18 : FONTSIZE.size_20}
+              <View style={styles.ItemPropertiesContainer}>
+                <View style={styles.ProperFirst}>
+                  <MaterialCommunityIcons
+                    name={type == 'Bean' ? 'alpha-b-circle' : 'coffee'}
+                    size={type == 'Bean' ? FONTSIZE.size_20 : FONTSIZE.size_24}
                     color={COLORS.primaryOrangeHex}
                   />
+                  <Text
+                    style={[
+                      styles.PropertyTextFirst,
+                      {
+                        marginTop: type == 'Bean' ? SPACING.space_4 + SPACING.space_2 : 0
+                      }
+                    ]}
+                  >
+                    {type}
+                  </Text>
                 </View>
+                <View style={styles.ProperFirst}>
+                  <Entypo
+                    name={type == 'Bean' ? 'location-pin' : 'drop'}
+                    size={FONTSIZE.size_16}
+                    color={COLORS.primaryOrangeHex}
+                  />
+                  <Text style={[styles.PropertyTextLast]}>{ingredients}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.InfoContainerRow}>
+              <View style={styles.RatingContainer}>
+                <Entypo name='star' color={COLORS.primaryOrangeHex} size={FONTSIZE.size_20}/>
+                <Text style={styles.RatingText}>{average_rating}</Text>
+                <Text style={styles.RatingCountText}>({ratings_count})</Text>
+              </View>
+              <View style={styles.RoastedContainer}>
+                <Text style={styles.RoastedText}>{roasted}</Text>
               </View>
             </View>
           </View>
@@ -142,12 +169,84 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end'
   },
-  ImageInfoOuterContainer:{},
-  ImageInfoInnerContainer:{},
-  InfoContainerRow:{},
-  ItemTitleText:{},
-  ItemSubtitleText:{},
-
+  ImageInfoOuterContainer: {
+    paddingVertical: SPACING.space_24,
+    paddingHorizontal: SPACING.space_30,
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopLeftRadius: BORDERRADIUS.radius_20 * 2,
+    borderTopRightRadius: BORDERRADIUS.radius_20 * 2,
+  },
+  ImageInfoInnerContainer: {
+    justifyContent: 'center',
+    gap: SPACING.space_15
+  },
+  InfoContainerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  ItemTitleText: {
+    fontFamily: 'Poppins-Semibold',
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.primaryWhiteHex,
+  },
+  ItemSubtitleText: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  ItemPropertiesContainer:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.space_20
+  },
+  ProperFirst:{
+    height: 55,
+    width: 55,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex
+  },
+  PropertyTextFirst: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  PropertyTextLast:{
+    fontFamily: 'Poppins-Medium',
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+    marginTop: SPACING.space_2 + SPACING.space_4,
+  },
+  RatingContainer:{
+    flexDirection: 'row',
+    gap: SPACING.space_10,
+    alignItems: 'center'
+  },
+  RatingText:{
+    fontFamily: 'Poppins-Semibold',
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex
+  },
+  RatingCountText:{
+    fontFamily: 'Poppins-Regular',
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex
+  },
+  RoastedContainer:{
+    height: 55,
+    width: 55 * 2 + SPACING.space_20,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex
+  },
+  RoastedText:{
+    fontFamily: 'Poppins-Regular',
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex
+  },
 })
 
 export default ImageBackgroundInfo
