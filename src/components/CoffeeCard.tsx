c7b4dbf8fa6e33cc3d4f973bcc2e09da0e97a96f
Savio <saviosoares0402@gmail.com>
2023-11-16 11:33:17 -0300

    edition coffeecard

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet, Text, View, Dimensions, ImageBackground, ImageProps, TouchableOpacity } from 'react-native'
 import React from 'react'
-import {LinearGradient} from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient';
 import { BORDERRADIUS, COLORS, FONTSIZE, SPACING } from '../theme/theme';
-import {AntDesign} from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons'
 import BGIcon from './BGIcon';
 
 const CARD_WIDTH = Dimensions.get('window').width * 0.32;
@@ -12,7 +12,7 @@ interface CoffeeCardProps {
     id: string;
     index: number;
     type: string;
-    rosted: string;
+    roasted: string;
     imagelink_square: ImageProps;
     name: string;
     special_igredient: string;
@@ -21,11 +21,11 @@ interface CoffeeCardProps {
     buttonPressHandler: any;
 }
 
-const CoffeeCard: React.FC <CoffeeCardProps> = ({
+const CoffeeCard: React.FC<CoffeeCardProps> = ({
     id,
     index,
     type,
-    rosted,
+    roasted,
     imagelink_square,
     name,
     special_igredient,
@@ -33,59 +33,72 @@ const CoffeeCard: React.FC <CoffeeCardProps> = ({
     price,
     buttonPressHandler,
 }) => {
-  return (
-    <LinearGradient
-        start={{x:0, y:0}}
-        end={{x:1, y:1}}
-        style={styles.CardLinearGradientContainer}
-        colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}>
-        <ImageBackground 
-            source={imagelink_square}
-            style={styles.CardImageBG}
-            resizeMode='cover'
-        >
-            <View style={styles.CardRatingContainer}>
-                <AntDesign 
-                    color={COLORS.primaryOrangeHex} 
-                    name='star'
-                    size={FONTSIZE.size_16}
-                />
-                <Text style={styles.CardRatingText}>{average_rating}</Text>
+    return (
+        <LinearGradient
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={styles.CardLinearGradientContainer}
+            colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}>
+            <ImageBackground
+                source={imagelink_square}
+                style={styles.CardImageBG}
+                resizeMode='cover'
+            >
+                <View style={styles.CardRatingContainer}>
+                    <AntDesign
+                        color={COLORS.primaryOrangeHex}
+                        name='star'
+                        size={FONTSIZE.size_16}
+                    />
+                    <Text style={styles.CardRatingText}>{average_rating}</Text>
+                </View>
+            </ImageBackground>
+            <Text style={styles.CardTitle}>{name}</Text>
+            <Text style={styles.CardSubTitle}>{special_igredient}</Text>
+            <View style={styles.CardFooterBow}>
+                <Text style={styles.CardPriceCurrency}>
+                    $ <Text style={styles.CardPrice}>{price.price}</Text>
+                </Text>
+                <TouchableOpacity onPress={() => {
+                    buttonPressHandler({
+                        id,
+                        index,
+                        type,
+                        roasted,
+                        imagelink_square,
+                        name,
+                        special_igredient,
+                        // average_rating,
+                        prices:[{...price, quantify: 1}],
+                        // buttonPressHandler,
+                    })
+                }}>
+                    <BGIcon
+                        name={'add'}
+                        color={COLORS.primaryWhiteHex}
+                        size={FONTSIZE.size_18}
+                        BGColor={COLORS.primaryOrangeHex}
+                    />
+                </TouchableOpacity>
             </View>
-        </ImageBackground>
-        <Text style={styles.CardTitle}>{name}</Text>
-        <Text style={styles.CardSubTitle}>{special_igredient}</Text>
-        <View style={styles.CardFooterBow}>
-            <Text style={styles.CardPriceCurrency}>
-                $ <Text style={styles.CardPrice}>{price.price}</Text>
-            </Text>
-            <TouchableOpacity onPress={() => {}}>
-                <BGIcon
-                    name={'add'}
-                    color={COLORS.primaryWhiteHex}
-                    size={FONTSIZE.size_18}
-                    BGColor={COLORS.primaryOrangeHex}
-                />
-            </TouchableOpacity>
-        </View>
-    </LinearGradient>
-  );
+        </LinearGradient>
+    );
 };
 
 
 const styles = StyleSheet.create({
-    CardLinearGradientContainer:{
+    CardLinearGradientContainer: {
         padding: SPACING.space_15,
         borderRadius: BORDERRADIUS.radius_25,
     },
-    CardImageBG:{
+    CardImageBG: {
         width: CARD_WIDTH,
         height: CARD_WIDTH,
         borderRadius: BORDERRADIUS.radius_20,
         marginBottom: SPACING.space_15,
         overflow: 'hidden',
     },
-    CardRatingContainer:{
+    CardRatingContainer: {
         flexDirection: 'row',
         backgroundColor: COLORS.primaryBlackHex,
         alignItems: 'center',
@@ -98,35 +111,35 @@ const styles = StyleSheet.create({
         top: 0,
         right: 0,
     },
-    CardRatingText:{
+    CardRatingText: {
         fontFamily: 'Poppins-Medium',
         color: COLORS.primaryWhiteHex,
         lineHeight: 22,
         fontSize: FONTSIZE.size_14,
     },
-    CardTitle:{
+    CardTitle: {
         fontFamily: 'Poppins-Medium',
         color: COLORS.primaryWhiteHex,
         fontSize: FONTSIZE.size_16,
     },
-    CardSubTitle:{
+    CardSubTitle: {
         // fontFamily: 'Poppins-Light',
         color: COLORS.primaryWhiteHex,
         fontSize: FONTSIZE.size_10,
     },
     CardFooterBow: {
-        flexDirection:'row',
+        flexDirection: 'row',
         justifyContent: 'space-between',
         // alignSelf: 'center',
         marginTop: SPACING.space_15,
         // alignSelf: 'stretch'
     },
-    CardPriceCurrency:{
+    CardPriceCurrency: {
         fontFamily: 'Poppins-Semibold',
         color: COLORS.primaryOrangeHex,
         fontSize: FONTSIZE.size_18
     },
-    CardPrice:{
+    CardPrice: {
         color: COLORS.primaryWhiteHex,
     },
 })
