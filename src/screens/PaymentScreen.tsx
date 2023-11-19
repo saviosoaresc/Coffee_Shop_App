90d78d411f98a5ac98c91035a12c927fc78b8939
Savio <saviosoares0402@gmail.com>
2023-11-19 10:45:34 -0300

    edition payscre

---
+++ src/screens/PaymentScreen.tsx
@@ -0,0 +1,267 @@
+import React, { useState } from 'react'
+import {
+  StyleSheet, Text, View,
+  StatusBar, ScrollView, TouchableOpacity
+} from 'react-native'
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import PopUpAnimation from '../components/PopUpAnimation';
+import PaymentFooter from '../components/PaymentFooter';
+import { LinearGradient } from 'expo-linear-gradient';
+import GradientBGIcon from '../components/GradientBGIcon';
+import { useStore } from '../store/store';
+import PaymentMethod from '../components/PaymentMethod';
+import {MaterialCommunityIcons, Fontisto} from '@expo/vector-icons'
+
+const PaymentList = [
+  {
+    name: 'Wallet',
+    // problema nesse icone
+    icon: 'icon',
+    isIcons: true
+  },
+  {
+    name: 'Google Pay',
+    icon: require('../assets/app_images/gpay.png'),
+    isIcons: false
+  },
+  {
+    name: 'Apple Pay',
+    icon: require('../assets/app_images/applepay.png'),
+    isIcons: false
+  },
+  {
+    name: 'Amaz. Pay',
+    icon: require('../assets/app_images/amazonpay.png'),
+    isIcons: false
+  },
+]
+
+const PaymentScreen = ({navigation, route}: any) => {
+
+  const calculateCartPrice = useStore((state: any) => state.calculateCartPrice);
+  const addToOrderHistoryListFromCart = useStore((state: any) => state.addToOrderHistoryListFromCart);
+
+  const [paymentMode, setPaymentMode] = useState('Credit Card');
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  const buttonPressHandler = () =>{
+    setShowAnimation(true);
+    addToOrderHistoryListFromCart();
+    calculateCartPrice();
+    setTimeout(()=>{
+      setShowAnimation(false);
+      navigation.navigate('History');
+      },2000);
+  }
+
+  return (
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex}/>
+      {showAnimation ? (
+        <PopUpAnimation
+          style={styles.ScreenContainer}
+          source={require('../lottie/successful.json')}
+        />
+      ) : (
+        <></>
+      )}
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}
+      >
+        <View style={styles.HeaderContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.pop();
+            }}
+          >
+            <GradientBGIcon name='chevron-left' color={COLORS.primaryLightGreyHex} size={FONTSIZE.size_16}/>
+          </TouchableOpacity>
+          <Text style={styles.HeaderText}>Payment</Text>
+          <View style={styles.EmptyView}/>
+        </View>
+
+
+        <View style={styles.PaymentOptionsContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              setPaymentMode('Credit Card')
+            }}
+          >
+            <View
+              style={[
+                styles.CreditCardContainer,
+                {
+                  borderColor:
+                    paymentMode == 'Credit Card'
+                      ? COLORS.primaryOrangeHex
+                      : COLORS.primaryGreyHex,
+                },
+              ]}
+            >
+              <Text style={styles.CreditCardTitle}>Credit Card</Text>
+              <View style={styles.CreditCardBG}>
+                <LinearGradient
+                  start={{x: 0, y: 0}}
+                  end={{x: 1, y: 1}}
+                  style={styles.LinearGradientStyle}
+                  colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}
+                >
+                  <View style={styles.CreditCardRow}>
+                    <MaterialCommunityIcons
+                      name='credit-card-chip'
+                      size={FONTSIZE.size_20 * 2}
+                      color={COLORS.primaryOrangeHex}
+                    />
+                    <Fontisto
+                      name='visa'
+                      size={FONTSIZE.size_30 * 2}
+                      color={COLORS.primaryWhiteHex}
+                    />
+                  </View>
+                  <View style={styles.CreditCardNumberContainer}>
+                    <Text style={styles.CreditCardNumber}>0001</Text>
+                    <Text style={styles.CreditCardNumber}>1222</Text>
+                    <Text style={styles.CreditCardNumber}>3444</Text>
+                    <Text style={styles.CreditCardNumber}>5556</Text>
+                  </View>
+                  <View style={styles.CreditCardRow}>
+                    <View style={styles.CreditCardNameContainer}>
+                      <Text style={styles.CreditCardNameSubitle}>
+                        Card Holder Name
+                      </Text>
+                      <Text style={styles.CreditCardNameTitle}>
+                        SAVIO C SOARES
+                      </Text>
+                    </View>
+                    <View style={styles.CreditCardDateContainer}>
+                      <Text style={styles.CreditCardNameSubitle}>
+                        Expiry Date
+                      </Text>
+                      <Text style={styles.CreditCardNameTitle}>02/30</Text>
+                    </View>
+                  </View>
+                </LinearGradient>
+              </View>
+            </View>
+          </TouchableOpacity>
+          {PaymentList.map((data: any) => (
+            <TouchableOpacity
+              key={data.name}
+              onPress={()=>{setPaymentMode(data.name)}}
+            >
+              <PaymentMethod
+                paymentMode={paymentMode}
+                name={data.name}
+                icon={data.icon}
+                isIcon={data.isIcon}
+              />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
+      <PaymentFooter
+        buttonTitle={`Pay with ${paymentMode}`}
+        price={{price: route.params.amount, currency: '$'}}
+        buttonPressHandler={buttonPressHandler}
+      />
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  LottieAnimation: {
+    flex: 1,
+  },
+  ScrollViewFlex: {
+    flexGrow: 0,
+  },
+  HeaderContainer: {
+    paddingHorizontal: SPACING.space_24,
+    paddingVertical: SPACING.space_15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: '6%'
+  },
+  HeaderText: {
+    fontFamily: 'Poppins-Semibold',
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.primaryWhiteHex,
+  },
+  EmptyView: {
+    height: SPACING.space_36,
+    width: SPACING.space_36,
+  },
+  PaymentOptionsContainer: {
+    padding: SPACING.space_15,
+    gap: SPACING.space_15,
+  },
+  CreditCardContainer: {
+    padding: SPACING.space_10,
+    gap: SPACING.space_10,
+    borderRadius: BORDERRADIUS.radius_15 * 2,
+    borderWidth: 3,
+  },
+  CreditCardTitle: {
+    fontFamily: 'Poppins-Semibold',
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.primaryWhiteHex,
+    marginLeft: SPACING.space_10,
+  },
+  CreditCardBG: {
+    backgroundColor: COLORS.primaryGreyHex,
+    borderRadius: BORDERRADIUS.radius_25,
+  },
+  LinearGradientStyle: {
+    borderRadius: BORDERRADIUS.radius_25,
+    gap: SPACING.space_36,
+    paddingHorizontal: SPACING.space_15,
+    paddingVertical: SPACING.space_10,
+  },
+  CreditCardRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  CreditCardNumberContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_10,
+    alignItems: 'center',
+  },
+  CreditCardNumber: {
+    fontFamily: 'Poppins-Semibold',
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+    letterSpacing: SPACING.space_4 + SPACING.space_2,
+  },
+  CreditCardNameSubitle: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.secondaryLightGreyHex,
+  },
+  CreditCardNameTitle: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+  CreditCardNameContainer: {
+    alignItems: 'flex-start',
+  },
+  CreditCardDateContainer: {
+    alignItems: 'flex-end',
+  },
+})
+
+export default PaymentScreen
