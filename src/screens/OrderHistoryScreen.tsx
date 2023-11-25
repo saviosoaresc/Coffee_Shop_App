950765219037787508d498de080fbd61cc5358f0
Savio <saviosoares0402@gmail.com>
2023-11-25 17:12:34 -0300

    edition orderhistorys

--- src/screens/OrderHistoryScreen.tsx
+++ src/screens/OrderHistoryScreen.tsx
@@ -1,15 +1,132 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  StyleSheet, Text, View,
+  StatusBar, ScrollView, TouchableOpacity
+} from 'react-native'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import HeaderBar from '../components/HeaderBar';
+import EmptyListAnimation from '../components/EmptyListAnimation';
+import PopUpAnimation from '../components/PopUpAnimation';
+import OrderHistoryCard from '../components/OrderHistoryCard';
+import { useStore } from '../store/store';
 
-const OrderHistoryScreen = () => {
+
+const OrderHistoryScreen = ({ navigation }: any) => {
+  const OrderHistoryList = useStore((state: any) => state.OrderHistoryList);
+  const removeAllItemsInOrderHistoryList = useStore((state: any) => state.removeAllItemsInOrderHistoryList);
+  const tabBarHeight = useBottomTabBarHeight();
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  const navigationHandler = ({ index, id, type }: any) => {
+    navigation.push('Details', {
+      index, id, type,
+    })
+  }
+
+  const buttonPressHandler = () => {
+    setShowAnimation(true);
+    setTimeout(() => {
+      setShowAnimation(false);
+      removeAllItemsInOrderHistoryList()
+    }, 3000);
+  };
+
   return (
-    <View>
-      <Text>OrderHistoryScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      {showAnimation ? (
+        <PopUpAnimation
+          style={styles.LottieAnimation}
+          source={require('../lottie/coffeeDownload.json')}
+        />
+      ) : (
+        <></>
+      )}
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}
+      >
+        <View
+          style={[styles.ScrollViewInnerView, { marginBottom: tabBarHeight }]}>
+          <HeaderBar title="Historico de Pedidos" />
+          {/* if there is no data in the list */}
+          <View style={styles.ItemContainer}>
+            {OrderHistoryList.length == 0 ? (
+              <EmptyListAnimation title={'No Order Histoy'} />
+            ) : (
+              <View style={styles.ListItemContainer}>
+            {/* // render all orders history cards */}
+                {OrderHistoryList.map((data: any, index: number) => (
+                  <OrderHistoryCard
+                    key={index.toString()}
+                    navigationHandler={navigationHandler}
+                    CartList={data.CartList}
+                    CartListPrice={data.CartListPrice}
+                    OrderDate={data.OrderDate} />
+                ))}
+              </View>
+            )}
+          </View>
+          {OrderHistoryList.length > 0 ? (
+            <TouchableOpacity
+              style={styles.DownloadButton}
+              onPress={() => {
+                buttonPressHandler();
+              }}>
+              <Text style={styles.ButtonText}>Download</Text>
+            </TouchableOpacity>
+          ) : (
+            <></>
+          )}
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  LottieAnimation: {
+    height: 250,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+  ScrollViewInnerView: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  ItemContainer: {
+    flex: 1,
+  },
+  ListItemContainer: {
+    paddingHorizontal: SPACING.space_20,
+    gap: SPACING.space_30,
+  },
+  DownloadButton: {
+    margin: SPACING.space_20,
+    backgroundColor: COLORS.primaryOrangeHex,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: SPACING.space_36 * 2,
+    borderRadius: BORDERRADIUS.radius_20,
+  },
+  ButtonText: {
+    fontFamily: 'Poppins-Semibold',
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+})
 
 export default OrderHistoryScreen
