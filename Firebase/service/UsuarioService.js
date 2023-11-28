465cbb92e8dffeda10b01a6a3224e108806ed2f6
Savio <saviosoares0402@gmail.com>
2023-11-28 13:27:18 -0300

    ediiton UsuarioService

---
+++ Firebase/service/UsuarioService.js
@@ -0,0 +1,19 @@
+// import { createUserWithEmailAndPassword} from "firebase/auth"
+
+// class UsuarioService{
+
+//     static signUp = (auth, email, password, callback) => {
+//         createUserWithEmailAndPassword(
+//             auth, email, password
+//         )
+//         .then(
+//             (userCredential) => {
+//                 callback(userCredential)
+//             }
+//             )
+//             .catch(
+//                 (error) => console.log(error)
+//         )
+//     }
+
+// }
