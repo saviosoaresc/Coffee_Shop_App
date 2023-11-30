e9d3ed77bcb29b7d205757f8e5c17efa2c618d60
Savio <saviosoares0402@gmail.com>
2023-11-29 22:38:42 -0300

    edition pay

--- src/components/PaymentFooter.tsx
+++ src/components/PaymentFooter.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import styles from './styles/PaymentFooterStyle';
 
 interface PriceProps{
     price: string;
@@ -35,47 +35,5 @@ const PaymentFooter: React.FC<PaymentFooterProps> = ({
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-    PriceFooter:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: SPACING.space_20,
-        marginBottom: '5%'
-        // padding: SPACING.space_18,
-    },
-    PriceContainer:{
-        alignItems: 'center',
-        width: 100,
-    },
-    PriceTitle:{
-        fontFamily: 'Poppins-Medium',
-        fontSize: FONTSIZE.size_14,
-        color: COLORS.secondaryLightGreyHex,
-    },
-    PriceText:{
-        fontFamily: 'Poppins-Semibold',
-        fontSize: FONTSIZE.size_24,
-        color: COLORS.primaryOrangeHex,
-    },
-    Price:{
-        color: COLORS.primaryWhiteHex
-    },
-    PayButton:{
-        backgroundColor: COLORS.primaryOrangeHex,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: BORDERRADIUS.radius_20,
-        height: SPACING.space_28 * 2,
-        width: '60%'
-
-    },
-    ButtonText:{
-        fontFamily: 'Poppins-Semibold',
-        fontSize: FONTSIZE.size_18,
-        color: COLORS.primaryWhiteHex
-    },
-})
 
 export default PaymentFooter
