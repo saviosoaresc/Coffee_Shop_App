b4cfcdb5170e6546977e7086f21c107cd02fb902
Savio <saviosoares0402@gmail.com>
2023-11-29 22:40:06 -0300

    edition details

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, StatusBar, ScrollView, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
+import { Text, View, StatusBar, ScrollView, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
 import { useStore } from '../store/store'
-import { BORDERRADIUS, COLORS, FONTSIZE, SPACING } from '../theme/theme';
 import ImageBackgroundInfo from '../components/ImageBackgroundInfo';
 import PaymentFooter from '../components/PaymentFooter';
-
-//erro ao desfavoritar
-//4:43:00
+import styles from './styles/DetailsScreenStyle';
+import { COLORS, FONTSIZE } from '../theme/theme';
 
 const DetailsScreen = ({ navigation, route }: any) => {
   const ItemOfIndex = useStore((state: any) =>
@@ -145,51 +143,5 @@ const DetailsScreen = ({ navigation, route }: any) => {
     </View>
   )
 }
-
-
-const styles = StyleSheet.create({
-  ScreenContainer: {
-    flex: 1,
-    backgroundColor: COLORS.primaryBlackHex,
-  },
-  ScrollViewFlex: {
-    flexGrow: 1,
-    justifyContent: 'space-between'
-  },
-  FooterInfoArea: {
-    padding: SPACING.space_20
-  },
-  InfoTitle: {
-    fontFamily: 'Poppins-Semibold',
-    fontSize: FONTSIZE.size_16,
-    color: COLORS.primaryWhiteHex,
-    marginBottom: SPACING.space_10
-  },
-  DescriptionText: {
-    letterSpacing: 0.5,
-    fontFamily: 'Poppins-Regular',
-    fontSize: FONTSIZE.size_14,
-    color: COLORS.primaryWhiteHex,
-    marginBottom: SPACING.space_30
-  },
-  SizeOuterContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    gap: SPACING.space_20
-  },
-  SizeBox: {
-    flex: 1,
-    backgroundColor: COLORS.primaryDarkGreyHex,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: SPACING.space_24 * 2,
-    borderRadius: BORDERRADIUS.radius_10,
-    borderWidth: 2
-  },
-  SizeText: {
-    fontFamily: 'Poppins-Medium'
-  },
-})
 
 export default DetailsScreen
