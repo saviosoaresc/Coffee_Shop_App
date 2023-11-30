5b6c8d2ef79be0a422a86b4fbc258b373fb7d815
Savio <saviosoares0402@gmail.com>
2023-11-29 22:37:30 -0300

    edition coffeecard

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -1,15 +1,14 @@
 import {
-    StyleSheet, Text, View,
-    Dimensions, ImageBackground, ImageProps,
+     Text, View,
+     ImageBackground, ImageProps,
     TouchableOpacity
 } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
-import { BORDERRADIUS, COLORS, FONTSIZE, SPACING } from '../theme/theme';
+import { COLORS, FONTSIZE} from '../theme/theme';
 import { AntDesign } from '@expo/vector-icons'
 import BGIcon from './BGIcon';
-
-const CARD_WIDTH = Dimensions.get('window').width * 0.32;
+import styles from './styles/CoffeeCardStyle';
 
 
 interface CoffeeCardProps {
@@ -88,63 +87,5 @@ const CoffeeCard: React.FC<CoffeeCardProps> = ({
     );
 };
 
-
-const styles = StyleSheet.create({
-    CardLinearGradientContainer: {
-        padding: SPACING.space_15,
-        borderRadius: BORDERRADIUS.radius_25,
-    },
-    CardImageBG: {
-        width: CARD_WIDTH,
-        height: CARD_WIDTH,
-        borderRadius: BORDERRADIUS.radius_20,
-        marginBottom: SPACING.space_15,
-        overflow: 'hidden',
-    },
-    CardRatingContainer: {
-        flexDirection: 'row',
-        backgroundColor: COLORS.primaryBlackHex,
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: SPACING.space_10,
-        paddingHorizontal: SPACING.space_15,
-        position: 'absolute',
-        borderBottomLeftRadius: BORDERRADIUS.radius_20,
-        borderTopRightRadius: BORDERRADIUS.radius_20,
-        top: 0,
-        right: 0,
-    },
-    CardRatingText: {
-        fontFamily: 'Poppins-Medium',
-        color: COLORS.primaryWhiteHex,
-        lineHeight: 22,
-        fontSize: FONTSIZE.size_14,
-    },
-    CardTitle: {
-        fontFamily: 'Poppins-Medium',
-        color: COLORS.primaryWhiteHex,
-        fontSize: FONTSIZE.size_16,
-    },
-    CardSubTitle: {
-        // fontFamily: 'Poppins-Light',
-        color: COLORS.primaryWhiteHex,
-        fontSize: FONTSIZE.size_10,
-    },
-    CardFooterBow: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        // alignSelf: 'center',
-        marginTop: SPACING.space_15,
-        // alignSelf: 'stretch'
-    },
-    CardPriceCurrency: {
-        fontFamily: 'Poppins-Semibold',
-        color: COLORS.primaryOrangeHex,
-        fontSize: FONTSIZE.size_18
-    },
-    CardPrice: {
-        color: COLORS.primaryWhiteHex,
-    },
-})
 
 export default CoffeeCard
