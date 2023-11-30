0c41e0dd3f4b74239bc5cba0cb2f96d61851b48f
Savio <saviosoares0402@gmail.com>
2023-11-29 22:41:14 -0300

    edition favorite

---
+++ src/screens/styles/FavoriteScreenStyle.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native"
+import { COLORS, SPACING } from "../../theme/theme"
+
+const styles = StyleSheet.create({
+    ScreenContainer: {
+      flex: 1,
+      backgroundColor: COLORS.primaryBlackHex,
+    },
+    ScrollViewFlex: {
+      flexGrow: 1,
+    },
+    ScrollViewInnerView: {
+      flex: 1,
+      justifyContent: 'space-between',
+    },
+    ItemContainer: {
+      flex: 1,
+    },
+    ListItemContainer: {
+      paddingHorizontal: SPACING.space_20,
+      gap: SPACING.space_20,
+    },
+  })
+
+  export default styles
