1a38b51d16082ec143d2d2d632c93e6d3fb5b80b
Savio <saviosoares0402@gmail.com>
2023-11-29 22:37:53 -0300

    edition header

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,48 +1,114 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Image, Alert } from 'react-native'
+import { COLORS, FONTSIZE, SPACING } from '../theme/theme';
 import useLoadFonts from '../hooks/useLoadFonts';
 import * as SplashScreen from 'expo-splash-screen'
 import GradientBGIcon from './GradientBGIcon';
 import ProfilePic from './ProfilePic';
+import { auth } from "../../Firebase/firebase/firebaseConfig";
+import { signOut } from "firebase/auth";
+import { BlurView } from "expo-blur";
+import { AntDesign } from "@expo/vector-icons";
 
 SplashScreen.preventAutoHideAsync();
-
 
 interface HearderBarProps {
     title?: string;
 }
 
-const HearderBar: React.FC<HearderBarProps> = ({title}) => {
-    const {fontsLoaded, onLayoutRootView} = useLoadFonts();
-    if(!fontsLoaded)
+const HearderBar: React.FC<HearderBarProps> = ({ title }) => {
+    const [modalVisible, setModalVisible] = useState(false);
+    const { fontsLoaded, onLayoutRootView } = useLoadFonts();
+    if (!fontsLoaded)
         return null;
 
+    function handleLogOut() {
+        signOut(auth)
+            .then(() => {
+                console.log(auth);
+                Alert.alert("USUARIO DESLOGADO", "Voce saiu do App, reseta-o e entre novamente para fazer um novo login!")
+            })
+            .catch((error) => console.log(error));
+    }
+
     return (
-    <View style={styles.HearderContainer}>
-        <GradientBGIcon name='menu' color={COLORS.primaryLightGreyHex} size={FONTSIZE.size_28}/>
-        <Text style={styles.HeaderText}>{title}</Text>
-        <ProfilePic/>
-    </View>
-  )
+        <View style={styles.HearderContainer}>
+            <TouchableOpacity onPress={() => {
+                handleLogOut();
+            }}>
+                <GradientBGIcon name='sign-out' color={COLORS.primaryLightGreyHex} size={26} />
+            </TouchableOpacity>
+            <Text style={styles.HeaderText}>{title}</Text>
+            <TouchableOpacity onPress={() => {
+                setModalVisible(!modalVisible);
+            }}>
+                <ProfilePic />
+            </TouchableOpacity>
+
+
+
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <BlurView style={StyleSheet.absoluteFill} intensity={36} tint="dark" />
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Image
+                            style={styles.image}
+                            source={{ uri: 'https://i.imgur.com/XfF6uLX.png' }}
+                        />
+                        <TouchableOpacity
+                            style={styles.closeButton}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <AntDesign name="closecircle" size={70} color={'white'}/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    )
 }
 
 
 const styles = StyleSheet.create({
-    HearderContainer:{
+    HearderContainer: {
         marginTop: '3%',
         padding: SPACING.space_24,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        // backgroundColor: 'red'
     },
-    HeaderText:{
+    HeaderText: {
         fontFamily: 'Poppins-Semibold',
         fontSize: FONTSIZE.size_20,
         color: COLORS.primaryWhiteHex,
     },
 
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: '22%'
+    },
+    modalView: {
+        // backgroundColor: "#6B6869",
+        alignItems: "center",
+        width: "94%",
+    },
+    image:{
+        height: '50%',
+        width: '94%',
+        borderRadius: 30
+    },
+    closeButton:{
+        marginTop: '30%'
+    }
 })
 
 export default HearderBar
