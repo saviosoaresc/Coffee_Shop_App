57435468198ef3290d5b2420f1f23c36fd5c062b
Savio <saviosoares0402@gmail.com>
2023-11-29 22:37:11 -0300

    edition Gradient

--- src/components/GradientBGIcon.tsx
+++ src/components/GradientBGIcon.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import {LinearGradient} from 'expo-linear-gradient'
+import { LinearGradient } from 'expo-linear-gradient'
 import { COLORS, SPACING } from '../theme/theme'
-import {Entypo} from '@expo/vector-icons'
-import CustomIcon from './CustomIcon';
+import { Entypo, Octicons, FontAwesome } from '@expo/vector-icons'
 
 interface GradientBGIconProps {
   name: string;
@@ -11,32 +10,39 @@ interface GradientBGIconProps {
   size: number;
 }
 
-const GradientBGIcon: React.FC<GradientBGIconProps> = ({name, color, size}) => {
+const GradientBGIcon: React.FC<GradientBGIconProps> = ({ name, color, size }: any) => {
   return (
     <View style={styles.Container}>
       <LinearGradient
-        start={{x:0, y:0}}
-        end={{x:1, y:1}}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
         colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}
         style={styles.linearGradientBG}>
-          <Entypo name={name} color={color} size={size}/>
-        </LinearGradient>
+        {name == 'sign-out' ? (
+          <FontAwesome name={name} color={color} size={size} />
+        )
+          : (
+            <Entypo name={name} color={color} size={size} />
+          )
+        }
+
+      </LinearGradient>
     </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-  Container:{
+  Container: {
     borderWidth: 2,
     borderColor: COLORS.secondaryDarkGreyHex,
     borderRadius: SPACING.space_12,
     alignItems: 'center',
-    justifyContent:'center',
+    justifyContent: 'center',
     backgroundColor: COLORS.secondaryDarkGreyHex,
     overflow: 'hidden',
   },
-  linearGradientBG:{
+  linearGradientBG: {
     height: SPACING.space_36,
     width: SPACING.space_36,
     alignItems: 'center',
